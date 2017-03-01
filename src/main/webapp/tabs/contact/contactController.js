94edd29b08ecb6a01f9ee74d9cbab021b7523f2d
Sasmita4 <sasmita.maharana555@gmail.com>
2017-03-01 20:33:48 +0530

    contact page added

--- src/main/webapp/tabs/contact/contactController.js
+++ src/main/webapp/tabs/contact/contactController.js
@@ -1,7 +1,12 @@
+//angular.module("myApp")
+//	.controller('contactController',['$scope','$state','$http','sendEmailService',
+//        function($scope,$state, $http,sendEmailService) {
+//		$scope.sendEmail = function(){
+//			sendEmailService.send($scope.contact);
+//		}
+//	}]);
+
 angular.module("myApp")
-	.controller('contactController',['$scope','entity','$state','$http','sendEmailService',
-        function($scope,$state, $http,sendEmailService) {
-		$scope.sendEmail = function(){
-			sendEmailService.send($scope.contact);
-		}
-	}]);
+	.controller('contactController',
+        function($scope) {
+	});
